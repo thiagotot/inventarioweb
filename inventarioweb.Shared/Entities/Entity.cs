ff671229a6e112f97d7733162b6aed3c4e7cf36b
thiagotot <tvsctj@gmail.com>
2017-05-01 18:34:42 -0300

    Primeiro commit

---
+++ inventarioweb.Shared/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidator;
+
+namespace inventarioweb.Shared.Entities
+{
+    public abstract class Entity : Notifiable
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
